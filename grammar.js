777d3678384ffdd2a9d4eab261aed350a85a537b
Vijay-boy <viju09159@gmail.com>
2026-07-01 15:42:11 +0530

    Create grammar.js

---
+++ grammar.js
@@ -0,0 +1,52 @@
+const grammarRules = [
+{
+wrong: "i am go to school yesterday",
+correct: "I went to school yesterday.",
+explanation: "Use 'went' because 'yesterday' indicates the past tense."
+},
+{
+wrong: "he don't",
+correct: "He doesn't",
+explanation: "Use 'doesn't' with he, she and it."
+},
+{
+wrong: "she go",
+correct: "She goes",
+explanation: "Use 'goes' with she."
+},
+{
+wrong: "i has",
+correct: "I have",
+explanation: "Use 'have' with I."
+},
+{
+wrong: "they is",
+correct: "They are",
+explanation: "Use 'are' with they."
+},
+{
+wrong: "we was",
+correct: "We were",
+explanation: "Use 'were' with we."
+},
+{
+wrong: "he are",
+correct: "He is",
+explanation: "Use 'is' with he."
+},
+{
+wrong: "i didn't went",
+correct: "I didn't go",
+explanation: "After 'didn't', always use the base verb."
+},
+{
+wrong: "she have",
+correct: "She has",
+explanation: "Use 'has' with she."
+},
+{
+wrong: "i can able",
+correct: "I can",
+explanation: "Don't use 'can' and 'able' together."
+}
+];
